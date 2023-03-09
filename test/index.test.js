c202f66c49574d538295c5c81875048a2a8328fa
mannybennett <mannybennett99@gmail.com>
2023-03-08 20:24:01 -0500

    finished

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,7 @@ const {
 } = require('../src')
 
 describe('API protocol practice', () => {
-// Note that is is not best practice to make actual API calls with 
+// Note that it is not best practice to make actual API calls with 
 // these tests. Normally, you would stub these calls or use nock 
 // to prevent an actual network call and mock the responses for 
 // unit testing. This structure is in place for practice only. 
